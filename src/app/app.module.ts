b2da6b1a103ed7a63628534f431f013815d8ee41
VeenaPD <veenapd4@gmail.com>
2019-07-06 17:25:40 +0530

    Initial scafolding added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ExpensesComponent } from './expenses/expenses.component';
+import { HomeComponent } from './home/home.component';
+import { SettingsComponent } from './settings/settings.component';
+import { SideMenuComponent } from './side-menu/side-menu.component';
+import { ExpenseDetailsComponent } from './expenses/expense-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ExpensesComponent,
+    HomeComponent,
+    SettingsComponent,
+    SideMenuComponent,
+    ExpenseDetailsComponent
   ],
   imports: [
     BrowserModule,
