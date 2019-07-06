a6f1b3c8f3aecca1f01cc1ea14641315963ec5b6
VeenaPD <veenapd4@gmail.com>
2019-07-06 18:31:05 +0530

    side menu component added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { SettingsComponent } from './settings/settings.component';
+import { ExpenseDetailsComponent } from './expenses/expense-details.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'home',
+    component: HomeComponent
+  },
+  {
+    path: 'expense/:id',
+    component: ExpenseDetailsComponent
+  },
+  {
+    path: 'settings',
+    component: SettingsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
